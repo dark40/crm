6571b6bf11824e9c755541dd3f739cacd247437f
Freddie Yang <dark_yty@hotmail.com>
2022-10-31 16:50:31 +1100

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -46,10 +46,10 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
-          <Route element={<Home />}>
-            <Route path="/*" element={<Dashboard />}></Route>
+          <Route path="/*" element={<Home />}>
+            {/* <Route path="/" element={<Dashboard />}></Route>
             <Route path="/cases" element={<CaseList />}></Route>
-            <Route path="/cases/:id" element={<CaseItem />}></Route>
+            <Route path="/cases/:id" element={<CaseItem />}></Route> */}
           </Route>
           <Route path="*" element={<NoMatch />}></Route>
         </Routes>
