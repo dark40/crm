7ba3ac48e64db434272a1c97b8f502362f1ae7c6
Freddie Yang <dark_yty@hotmail.com>
2022-10-31 16:59:12 +1100

    heroku

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.get('*', function (_, res) { res.sendFile(indexPath) });
+// app.get('*', function (_, res) { res.sendFile(indexPath) });
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
